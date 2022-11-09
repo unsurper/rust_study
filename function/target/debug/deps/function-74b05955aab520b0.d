2498951c762a738ad8b5150793c4726756b3db1f
ych <qq809638233@gmail.com>
2022-11-09 14:09:38 +0800

    third day by surper

--- function/target/debug/deps/function-74b05955aab520b0.d
+++
@@ -1,5 +0,0 @@
-C:\git\github\rust_project\function\target\debug\deps\function-74b05955aab520b0.rmeta: src\main.rs
-
-C:\git\github\rust_project\function\target\debug\deps\function-74b05955aab520b0.d: src\main.rs
-
-src\main.rs:
--- function/target/debug/deps/function-b6f1ed5a5a9e22ff.d
+++
@@ -1,5 +0,0 @@
-C:\git\github\rust_project\function\target\debug\deps\function-b6f1ed5a5a9e22ff.rmeta: src\main.rs
-
-C:\git\github\rust_project\function\target\debug\deps\function-b6f1ed5a5a9e22ff.d: src\main.rs
-
-src\main.rs:
